2872c41e0e939b9568494e2439f61050ce72cb11
Your Name <eliisa.heino@myy.haaga-helia.fi>
2023-01-29 17:35:03 +0200

    valmis 29.1.2023

--- App.js
+++ App.js
@@ -1,10 +1,78 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+
 
 export default function App() {
+  
+    //Annettut numerot tallennetaan number-tilamuuttujiin
+    //viesti-tilamuuttujaan asetetaan vastaus ja näytetään se käyttöliittymässä
+    //data on flatlistiä varten
+    const [number1, setNumber1] = useState ('');
+    const [number2, setNumber2] = useState ('');
+    const [viesti, setViesti] = useState ('');
+    const [data, setData]  = useState ([]);
+
+    //Vastauksen talletukseen
+    let result;
+
+   
+     //funktio suorittaa + laskutoimituksen
+     const pressedPlus = () => {
+      result = parseInt(number1) + parseInt(number2);
+      setViesti (result)
+      pressedAdd()
+     }
+
+     //funktio suorittaa - laskutoimituksen
+        const pressedMinus = () => {
+          result = parseInt(number1) - parseInt(number2);
+          setViesti (result) 
+          pressedAdd()  
+      }
+
+      //Funktio lisää laskutoimituksen tuloksen listaan
+       const pressedAdd = () => {
+        setData([...data, { key: result}]);
+        setNumber1('');
+        setNumber2('');
+     }
+
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style ={styles.container}>
+      <Text style ={styles.text}>Calculator</Text>
+      <Text style ={styles.text}> Result: {viesti} </Text>
+
+      {/*2 syöttökenttää numeroille */}
+      {/*onChange antaa muuttujalle sisällön */}
+      <TextInput 
+          style={styles.input} 
+          onChangeText= {number1 => setNumber1(number1)}
+          value={number1}
+          keyboardType="numeric"/>
+      <TextInput 
+          style={styles.input}
+          onChangeText= {number2 => setNumber2(number2)} 
+          value={number2} 
+          keyboardType="numeric"/>   
+      </View>
+      <View style ={styles.container2}>
+      {/*Painikkeet plus ja miinuslaskuille */}    
+      <Button onPress={pressedPlus} title= "+" />
+      <Button onPress={pressedMinus} title= "-" />
+      </View>
+      <Text style={styles.text}>History</Text>
+
+      {/*Flatlist listan näyttämiseen */}
+      <FlatList 
+        data={data}
+        renderItem={({item}) => <Text style ={styles.item}>{item.key}</Text>}
+        keyExtractor ={(item, index) => index.toString() } />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +85,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  input : {
+    width:200  , 
+    marginTop: 4,
+    height: 30,
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+  text:{
+    fontSize: 20,
+    
+  },
+  item: {
+    padding : 5,
+    fontSize: 17,
+    marginTop: 1,
+  },
+  
 });
